6f827a383de76bdde3428ba305aeb654e3605b1e
Jaseincyberspace <jase.norton@gmail.com>
2020-04-19 17:02:56 +1200

    Custom dialog box has been created for when username or password are incorrect. Clicking 'try again' dismisses the dialog and clicking 'reset password' adds a message to the log. Code needs to be added for the 'reset password' button to open the reset password activity.

--- app/src/main/java/com/example/bit603_assignment2_jasonnorton_5001428/MainActivity.java
+++ app/src/main/java/com/example/bit603_assignment2_jasonnorton_5001428/MainActivity.java
@@ -1,18 +1,14 @@
 package com.example.bit603_assignment2_jasonnorton_5001428;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.DialogInterface;
-import android.icu.lang.UCharacterEnums;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
     public String TAG = "loginScreenLOGS";
@@ -27,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         final EditText password = findViewById(R.id.editText_password);
         final Button button_login = findViewById(R.id.button_login);
 
+        // Deal with button clicks:
         forgotPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,25 +41,40 @@ public class MainActivity extends AppCompatActivity {
     }// end onCreate method
 
     private void dialogBoxLoginFailed() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        // Create alert builder:
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
-        builder.setTitle(R.string.login_incorrect_login_details);
-        builder.setPositiveButton(R.string.login_try_again, new DialogInterface.OnClickListener() {
+        // Set custom layout for dialog box:
+        LayoutInflater layoutInflater = getLayoutInflater();
+        View loginFailedDialogLayout = layoutInflater.inflate(R.layout.login_failed_dialog_box, null);
+        builder.setView(loginFailedDialogLayout);
+
+        // Add buttons:
+        final Button button_tryAgain = loginFailedDialogLayout.findViewById(R.id.button_tryAgain);
+        final Button button_resetPassword = loginFailedDialogLayout.findViewById(R.id.button_resetPassword);
+
+        // Create dialog box and display it on screen:
+        final AlertDialog dialog = builder.create();
+
+        // Deal with button clicks:
+        button_tryAgain.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Log.d(TAG, "User chose to try logging in again");
+            public void onClick(View v) {
+                dialog.dismiss();
             }
         });
-        builder.setNegativeButton(R.string.login_reset_password, new DialogInterface.OnClickListener() {
+
+        button_resetPassword.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Log.d(TAG, "User chose to reset password");
+            public void onClick(View v) {
+                Log.d(TAG, "User would like to reset their password. **** Programmer needs to add code to switch to the Reset Password Activity");
             }
         });
 
-        AlertDialog dialog = builder.create();
+        // Show dialog box on screen:
         dialog.show();
-    }
+
+    }// end dialogBoxLoginFailed method
 
 }// end MainActivity
 
