1b3adb5a95de543c474f9f9a49113335b5f667c2
Jaseincyberspace <jase.norton@gmail.com>
2020-04-23 15:10:14 +1200

    Added Dialogbox to forgot password activity If username or favourite color are entered incorrectly a custom dialog box is displayed with a 'Try again' button

--- app/src/main/java/com/example/bit603_assignment2_jasonnorton_5001428/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/bit603_assignment2_jasonnorton_5001428/ForgotPasswordActivity.java
@@ -4,9 +4,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,38 +30,30 @@ public class ForgotPasswordActivity extends AppCompatActivity {
                 if(User.securityQuestionCorrect(editText_username.getText().toString(), editText_favouriteColour.getText().toString())) {
                     textView_password.setText(activeUser[2]);
                 }
-                // else{}
+                else{
+                    dialogBoxIncorrectUserCredentials();
+                }
             }
         });
 
     }// end onCreate method
 
-    private void dialogBoxIncorrectSecurityQuestion() {
+    private void dialogBoxIncorrectUserCredentials() {
         // Create alert builder:
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
         // Set custom layout for dialog box:
         LayoutInflater layoutInflater = getLayoutInflater();
-        View incorrectSecurityQuestionDialogLayout = layoutInflater.inflate(R.layout.incorrect_security_question_dialog_box, null);
-        builder.setView(incorrectSecurityQuestionDialogLayout);
+        View incorrectUserCredentialsDialogLayout = layoutInflater.inflate(R.layout.incorrect_user_credentials_dialog_box, null);
+        builder.setView(incorrectUserCredentialsDialogLayout);
 
         // Add buttons:
-        final Button button_confirmSignOut = incorrectSecurityQuestionDialogLayout.findViewById(R.id.button_confirmSignOut);
-        final Button button_cancelSignOut = incorrectSecurityQuestionDialogLayout.findViewById(R.id.button_cancelSignOut);
+        final Button button_tryAgain = incorrectUserCredentialsDialogLayout.findViewById(R.id.button_tryAgain);
 
         // Create dialog box and display it on screen:
         final AlertDialog dialog = builder.create();
 
-        // Deal with button clicks:
-        button_confirmSignOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(i);
-            }
-        });
-
-        button_cancelSignOut.setOnClickListener(new View.OnClickListener() {
+        button_tryAgain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dialog.dismiss();
@@ -74,42 +64,5 @@ public class ForgotPasswordActivity extends AppCompatActivity {
         dialog.show();
 
     }// end dialogBoxSignOut method
-
-    private void dialogBoxShowPassword() {
-        // Create alert builder:
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-        // Set custom layout for dialog box:
-        LayoutInflater layoutInflater = getLayoutInflater();
-        View passwordResetDialogLayout = layoutInflater.inflate(R.layout.password_hint_dialog_box, null);
-        builder.setView(passwordResetDialogLayout);
-
-        // Add buttons:
-        final Button button_confirmSignOut = passwordResetDialogLayout.findViewById(R.id.button_confirmSignOut);
-        final Button button_cancelSignOut = passwordResetDialogLayout.findViewById(R.id.button_cancelSignOut);
-
-        // Create dialog box and display it on screen:
-        final AlertDialog dialog = builder.create();
-
-        // Deal with button clicks:
-        button_confirmSignOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(i);
-            }
-        });
-
-        button_cancelSignOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialog.dismiss();
-            }
-        });
-
-        // Show dialog box on screen:
-        dialog.show();
-
-    }// end dialogBoxShowPassword method
 
 }// end ForgotPasswordActivity
